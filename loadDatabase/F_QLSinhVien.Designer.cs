729f4edbad065696b9fff71e06fd491e3033d60e
Thuan17 <thuanhuynh941@gmail.com>
2023-10-25 15:30:50 +0700

    update

--- loadDatabase/F_QLSinhVien.Designer.cs
+++ loadDatabase/F_QLSinhVien.Designer.cs
@@ -31,12 +31,13 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(F_QLSinhVien));
             this.DGVLSv = new System.Windows.Forms.DataGridView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.txtMSSV = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnTim = new System.Windows.Forms.Button();
+            this.txtClass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.DGVLSv)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -44,99 +45,110 @@
             // DGVLSv
             // 
             this.DGVLSv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DGVLSv.Location = new System.Drawing.Point(126, 237);
+            this.DGVLSv.Location = new System.Drawing.Point(95, 142);
             this.DGVLSv.Name = "DGVLSv";
             this.DGVLSv.RowHeadersWidth = 51;
             this.DGVLSv.RowTemplate.Height = 24;
-            this.DGVLSv.Size = new System.Drawing.Size(876, 418);
+            this.DGVLSv.Size = new System.Drawing.Size(876, 383);
             this.DGVLSv.TabIndex = 9;
+            this.DGVLSv.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGVLSv_CellClick);
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.txtMSSV);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnTim);
+            this.groupBox1.Controls.Add(this.txtClass);
             this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.txtName);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Location = new System.Drawing.Point(161, 122);
+            this.groupBox1.Location = new System.Drawing.Point(95, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(876, 89);
+            this.groupBox1.Size = new System.Drawing.Size(876, 124);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Thông tin ";
             // 
-            // button1
+            // txtMSSV
             // 
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(801, 21);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(55, 38);
-            this.button1.TabIndex = 5;
-            this.button1.UseVisualStyleBackColor = true;
+            this.txtMSSV.Location = new System.Drawing.Point(165, 66);
+            this.txtMSSV.Multiline = true;
+            this.txtMSSV.Name = "txtMSSV";
+            this.txtMSSV.Size = new System.Drawing.Size(246, 38);
+            this.txtMSSV.TabIndex = 6;
             // 
-            // textBox2
+            // label1
             // 
-            this.textBox2.Location = new System.Drawing.Point(526, 21);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(246, 38);
-            this.textBox2.TabIndex = 3;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "MSSV";
+            // 
+            // btnTim
+            // 
+            this.btnTim.FlatAppearance.BorderSize = 0;
+            this.btnTim.Image = ((System.Drawing.Image)(resources.GetObject("btnTim.Image")));
+            this.btnTim.Location = new System.Drawing.Point(801, 21);
+            this.btnTim.Name = "btnTim";
+            this.btnTim.Size = new System.Drawing.Size(55, 38);
+            this.btnTim.TabIndex = 5;
+            this.btnTim.UseVisualStyleBackColor = true;
+            this.btnTim.Click += new System.EventHandler(this.btnTim_Click);
+            // 
+            // txtClass
+            // 
+            this.txtClass.Location = new System.Drawing.Point(472, 12);
+            this.txtClass.Multiline = true;
+            this.txtClass.Name = "txtClass";
+            this.txtClass.Size = new System.Drawing.Size(246, 38);
+            this.txtClass.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(441, 34);
+            this.label3.Location = new System.Drawing.Point(417, 25);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(61, 25);
             this.label3.TabIndex = 4;
             this.label3.Text = "Lớp : ";
             // 
-            // textBox1
+            // txtName
             // 
-            this.textBox1.Location = new System.Drawing.Point(178, 21);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(246, 38);
-            this.textBox1.TabIndex = 1;
+            this.txtName.Location = new System.Drawing.Point(165, 12);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(246, 38);
+            this.txtName.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(19, 34);
+            this.label2.Location = new System.Drawing.Point(6, 25);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(153, 25);
             this.label2.TabIndex = 2;
             this.label2.Text = "Tên Sinh Viên : ";
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(380, 60);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(268, 32);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Quản Lý Sinh Viên";
-            // 
             // F_QLSinhVien
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1198, 723);
+            this.ClientSize = new System.Drawing.Size(1052, 546);
             this.Controls.Add(this.DGVLSv);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "F_QLSinhVien";
             this.Text = "F_QLSinhVien";
+            this.Load += new System.EventHandler(this.F_QLSinhVien_Load);
             ((System.ComponentModel.ISupportInitialize)(this.DGVLSv)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -144,11 +156,12 @@
 
         private System.Windows.Forms.DataGridView DGVLSv;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnTim;
+        private System.Windows.Forms.TextBox txtClass;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtName;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMSSV;
         private System.Windows.Forms.Label label1;
     }
 }
--- loadDatabase/F_QLSinhVien.cs
+++ loadDatabase/F_QLSinhVien.cs
@@ -2,19 +2,108 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using DTO;
 namespace loadDatabase
 {
     public partial class F_QLSinhVien : Form
     {
+        DB connection;
+        DataColumn[] primaryKey;
+        SqlDataAdapter adapter1, adapter2;
+
+
+        //SqlConnection con;
+        //SqlCommand cmd;
+        //string str = " Data Source=LAPTOP-ALA9RHJO\\SQLEXPRESS; Initial Catalog=QLSinhVien ;Integrated Security=True ";
+        //SqlDataAdapter adapter = new SqlDataAdapter();
+        //DataTable table = new DataTable();
         public F_QLSinhVien()
         {
             InitializeComponent();
+           
+
+            connection = new DB();
+            primaryKey = new DataColumn[1];
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void F_QLSinhVien_Load(object sender, EventArgs e)
+        {
+            //LoadDGVChuyenXe();
+        }
+
+        private void DGVLSv_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i = DGVLSv.CurrentRow.Index;
+            txtName.Text = DGVLSv.Rows[i].Cells[0].Value.ToString();
+
+            txtClass.Text = DGVLSv.Rows[i].Cells[1].Value.ToString();
+            
+        }
+
+        void LoadDGVChuyenXe()
+        {
+            try {
+                string ma = "select * From SinhVien";
+                var table = connection.GetDataTable(ma, "SinhVien");
+                DGVLSv.DataSource = table;
+            }
+            catch(Exception ex) {
+                MessageBox.Show("ooix");
+            } 
+        }
+
+        private void btnTim_Click(object sender, EventArgs e)
+        {
+            TimSinhVien();
+        }
+
+        void TimSinhVien()
+        {
+            //cmd = con.CreateCommand();
+            //cmd.CommandText = "SELect * from SinhVien where Masv like N'" + txtMSSV.Text + "'";
+            //adapter.SelectCommand = cmd;
+            //table.Clear();
+            //adapter.Fill(table);
+            //DGVLSv.DataSource = table;
+
+
+
+            void loadDGView()
+            {
+
+                try
+                {
+                    string selection = "SELect * from SinhVien where Masv like N'" + txtMSSV.Text + "'";
+                    var table = connection.GetDataTable(selection, "Ve");
+                    DGVLSv.DataSource = table;
+                    DGVLSv.Columns[0].HeaderText = "Mã SV";
+                    DGVLSv.Columns[1].HeaderText = "Ho Tên";
+                    DGVLSv.Columns[2].HeaderText = "Ngày SInh";
+                    DGVLSv.Columns[3].HeaderText = "Ma Lop";
+                   
+
+                }
+                catch
+                {
+                    MessageBox.Show("Không Load được dữ liệu");
+                }
+            }
+
+
+        }
+
+
+
     }
 }
